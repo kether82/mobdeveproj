a47fd6e74200dfb8793919935f49e8387e2e6c5e
VonChon <nvonrodriguez@gmail.com>
2021-08-27 00:19:02 +0800

    added initComponents, fab search button, and sort button

--- app/src/main/java/com/mobdeve/s11/lim/ivanjerwin/finalproject/MainActivity.java
+++ app/src/main/java/com/mobdeve/s11/lim/ivanjerwin/finalproject/MainActivity.java
@@ -1,14 +1,31 @@
 package com.mobdeve.s11.lim.ivanjerwin.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvNotes;
+
+    private ArrayList<Note> dataNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crudactivity);
+
+//        this.dataNotes =
+        this.initComponents();
+    }
+
+    private void initComponents(){
+        this.rvNotes = findViewById(R.id.rv_notes);
+        this.rvNotes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        this.rvNotes.setAdapter(new NoteAdapter(this.dataNotes));
     }
 }
