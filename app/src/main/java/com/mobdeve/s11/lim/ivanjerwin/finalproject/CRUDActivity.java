a3cf51a10bb44cfed50000cbebb4b5f72f4cb317
Ivan Lim <ivalim@ymail.com>
2021-08-26 11:33:33 +0800

    added note class

--- app/src/main/java/com/mobdeve/s11/lim/ivanjerwin/finalproject/CRUDActivity.java
+++ app/src/main/java/com/mobdeve/s11/lim/ivanjerwin/finalproject/CRUDActivity.java
@@ -118,7 +118,7 @@ public class CRUDActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // open camera(?)
-                Toast.makeText(getApplicationContext(),"Camera", Toast.LENGTH_SHORT);
+                Toast.makeText(CRUDActivity.this,"Camera", Toast.LENGTH_SHORT);
             }
         });
 
---
+++ app/src/main/java/com/mobdeve/s11/lim/ivanjerwin/finalproject/Note.java
@@ -0,0 +1,82 @@
+package com.mobdeve.s11.lim.ivanjerwin.finalproject;
+
+public class Note {
+    private String title;
+    private String content;
+    private byte[] image;
+    private boolean isFav;
+    private boolean isLocked;
+    private String password;
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isFav() {
+        return isFav;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public int isFavInt(){
+        if (isFav) return 1;
+        else return 0;
+    }
+
+    public int isLockedInt(){
+        if (isLocked) return 1;
+        else return 0;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setFav(boolean fav) {
+        isFav = fav;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+/**
+ private Context context;
+ private static final String DB_NAME ="NAVINotes.db";
+ private static final int DB_VER = 1;
+
+ private static final String TABLE_NAME = "notes";
+ private static final String COL_ID = "_id";
+ private static final String COL_TITLE = "note_title";
+ private static final String COL_CONTENT = "note_content";
+ private static final String COL_IMG = "note_img";
+ private static final String COL_IS_FAV = "note_fav";
+ private static final String COL_IS_LOCKED = "note_lock";
+ */
